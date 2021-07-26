481df0ae6618ea2294d0881503949b571dbd446f
SACCI <saccilucas@gmail.com>
2021-07-26 12:18:08 -0300

    error fix

--- teste.php
+++ teste.php
@@ -2,11 +2,14 @@
 
 require_once "vendor/autoload.php";
 use \AkaSacci\GetcnpjPhp\Search;
-
 $mySearch = new Search();
+$cnpj = "1"; //caso não haja nenhum dado no $_GET, a requisição é feita com um número planejado para dar erro.
 
-$resut = $mySearch->getDataFromCNPJ('52359692000162');
+if(isset($_GET['cnpj'])){
+$cnpj = $_GET['cnpj'];
+}
 
+$resut = $mySearch->getDataFromCNPJ($cnpj);
 print_r($resut);
 
 
