3af9a7e29ace8633e4834c0b068aa52a4eb34611
SACCI <saccilucas@gmail.com>
2021-07-27 20:21:26 -0300

    Exceptions update

--- src/Search.php
+++ src/Search.php
@@ -2,6 +2,8 @@
 
 namespace AkaSacci\GetcnpjPhp;
 
+use Exception;
+
 class Search {
 
     private $url = 'https://www.receitaws.com.br/v1/cnpj/';
@@ -9,8 +11,32 @@ class Search {
     public function getDataFromCNPJ(string $cnpj): array{
 
         $cnpj = preg_replace('/[^0-9]/im', '', $cnpj);
-        $getData = file_get_contents($this->url . $cnpj);
-        return (array) json_decode($getData);
+
+         try{
+             return $this->queryData($cnpj);
+         }
+         catch(Exception $e)
+         {
+            return $arrayError = array(
+                'status' => 'ERROR',
+                'message' => $e->getMessage(),
+                'errorCode' => $e->getCode()
+            );
+         }
+    }
+
+    public function queryData(string $cnpj): array{
+        $getData = @file_get_contents($this->url . $cnpj);//o uso de @ suprime o warning
+        if ($getData === FALSE) { 
+            
+            throw new Exception("Espere um minuto para tentar fazer uma nova requisição!", 101);
+
+        } else {
+
+            return (array) json_decode($getData);
+
+        }
+        
     }
 
 }
